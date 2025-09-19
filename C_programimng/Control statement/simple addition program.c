110b295741dc8ba39fe1b5d52e44ada3e2403431
saniya0101 <saniyashikalgar1234@gmail.com>
2025-09-19 19:37:41 +0530

    add

---
+++ C_programimng/Control statement/simple addition program.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a=0, b=0;
+printf("\n Enter number");
+printf("Enter a");
+scanf("%d",&a);
+printf("Enter b");
+scanf("%d",b);
+sum =a+b:
+printf("sum is %d",a+b);
+return 0;
+}
