08ba72d1bd8caf62ce48e5a2dd0003721ecfa5c3
saniya0101 <saniyashikalgar1234@gmail.com>
2025-10-02 16:58:32 +0530

    cube

---
+++ C_programimng/Control statement/cube of given number.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+
+int NO=0, cube=0;
+printf("\n Enter a number find its cube");
+scanf("%d",&NO);
+cube=NO*NO*NO;
+printf("\n cube of %d is =%d.",NO,cube);
+getch();
+return 0;
+}
+
